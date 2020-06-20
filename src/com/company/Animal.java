03e305b205706fd0fa708421780f13097d644bd5
Riivv <rks97@op.pl>
2020-06-20 14:00:43 +0200

    Task 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,7 +1,42 @@
 package com.company;
 
-public class Animal {
+public class Animal
+{
 
 
+    private double weight;
+    static final Double dog_weight = 12.0;
+    public Animal(String spiecies)
+    {
+        if(spiecies == "dog")
+            weight = dog_weight;
 
+    }
+    void feed()
+    {
+        if(this.weight<=0)
+        {
+            System.out.println("I'm dead idiot");
+        }
+        else
+            this.weight ++;
+            System.out.println("YES YES GIMME ME FOOOD, my weight is: "+ this.weight);
+    }
+    void takeForAWalk()
+    {
+        if(this.weight<=0)
+        {
+            System.out.println("I'm dead idiot");
+        }
+        else if(this.weight<=2)
+        {
+            this.weight--;
+            System.out.println("YES YES WALK WITH MA HUMIE, IMA HUNGRY GIMME FOOOOD, my weight is: " + this.weight);
+        }
+        else
+            {
+            this.weight--;
+            System.out.println("YES YES WALK WITH MA HUMIE, my weight is: " + this.weight);
+            }
+    }
 }
--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,6 @@
 package com.company;
 
 public class Human {
+    String Animal_pet;
+
 }
